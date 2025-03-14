d50a3026e2582f7894ef257e604367096997867e
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-03-14 21:55:58 +0900

    feat : chapter01, chapter02, chapter03 mission 추가

--- src/main/java/chapter01/mission/Application.java
+++ src/main/java/chapter01/mission/Application.java
@@ -2,20 +2,28 @@ package chapter01.mission;
 
 public class Application {
     public static void main(String[] args) {
-        String words = "Java is Fun";
-        System.out.println(calculateWordLength(words));
+        String s = "Java is Fun";
+        String result = Application.length(s);
+        System.out.println(result);
     }
 
-    public static String calculateWordLength(String words) {
-        /* TODO : 아래 각 항목의 코드를 구현해주세요. */
-        //1. 입력 문자열을 `split(" ")`으로 분할하여 단어 배열을 생성한다.
+    public static String length(String s) {
+//        1. 입력 문자열을 `split(" ")`으로 분할하여 단어 배열을 생성한다.
+        String[] word = s.split(" ");
 
-        //2. 배열의 인덱스 0, 1, 2의 단어를 변수에 직접 할당한다.
+//        2. 배열의 인덱스 0, 1, 2의 단어를 변수에 직접 할당한다.
+        String word0 = word[0];
+        String word1 = word[1];
+        String word2 = word[2];
 
-        //3. 각 단어의 길이를 `length()` 메서드로 계산한다.
+//        3. 각 단어의 길이를 `length()` 메서드로 계산한다.
+        int length1 = word0.length();
+        int length2 = word1.length();
+        int length3 = word2.length();
 
-        //4. 세 단어의 길이의 합계를 계산한다.
-        return "";
+//        4. 세 단어의 길이의 합계를 계산한다.
+        int result = length1 + length2 + length3;
+
+        return "단어1: " + length1 + ", 단어2: " + length2 + ", 단어3: " + length3 + ", 총 길이: " + result;
     }
-
 }
---
+++ src/main/java/chpater02/mission/Application.java
@@ -0,0 +1,49 @@
+package chpater02.mission;
+
+public class Application {
+    public static void main(String[] args) {
+        Application application = new Application();
+        String result = application.processWeek(new int[]{1, 3, 5, 8});
+        System.out.println(result);
+    }
+
+    public String processWeek(int[] input) {
+        int i = 0;
+        String week = "";
+//        1. for문을 사용하여 배열의 각 요소를 순차적으로 처리한다.
+        for (; i < input.length; i++) {
+
+//        2. switch 문을 사용하여 각 숫자에 해당하는 요일을 문자열로 변환한다.
+//        - 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"
+//                - 1~7 이외의 숫자는 "Unknown"으로 처리한다.
+            switch (input[i]) {
+                case 1:
+                    week += "Monday, ";
+                    break;
+                case 2:
+                    week += "Tuesday, ";
+                    break;
+                case 3:
+                    week += "Wednesday, ";
+                    break;
+                case 4:
+                    week += "Thursday, ";
+                    break;
+                case 5:
+                    week += "Friday, ";
+                    break;
+                case 6:
+                    week += "Saturday, ";
+                    break;
+                case 7:
+                    week += "Sunday, ";
+                    break;
+                default:
+                    week += "Unknown, ";
+            }
+        }
+
+//        3. 변환된 요일 문자열들을 콤마(,)로 구분하여 최종 결과 문자열을 생성한다.
+        return week.substring(0, week.length() - 2);
+    }
+}
