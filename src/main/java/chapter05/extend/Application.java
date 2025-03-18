99416af385c0fda2c04789291286467fe5a2daf6
www-author <dream970215@naver.com>
2025-03-18 17:53:29 +0900

    refactor : Application 출력 인스턴스 그룹화

--- src/main/java/chapter05/extend/Application.java
+++ src/main/java/chapter05/extend/Application.java
@@ -1,27 +1,27 @@
 package chapter05.extend;
 
 import chapter05.extend.model.*;
+import chapter05.extend.service.EngineService;
 
 /**
  * 이동 수단
  */
 public class Application {
     public static void main(String[] args) {
-        WheelBased car = new Car("자동차");
-        WheelBased bicycle = new Bicycle("자전거");
-        NonWheelBased ship = new Ship("배");
+        EngineService[] engines = {
+            new Car("자동차"),
+            new Bicycle("자전거"),
+            new Ship("배")
+        };
 
-        car.getWheelCount();
-        bicycle.getWheelCount();
-
-        ship.chargeBattery();
-
-        car.start();
-        ship.start();
-        bicycle.start();
-
-        car.stop();
-        ship.stop();
-        bicycle.stop();
+        for (EngineService engine : engines) {
+            engine.start();
+            if (engine instanceof NonWheelBased) {
+                ((NonWheelBased) engine).chargeBattery();
+            } else {
+                ((WheelBased) engine).getWheelCount();
+            }
+            engine.stop();
+        }
     }
 }
